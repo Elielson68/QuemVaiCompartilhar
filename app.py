d177be3de9eac84ca8e7ec31aff9d22cef3f6dc3
Elielson68 <elielsonbr.com@gmail.com>
2024-01-31 23:41:26 -0300

    testando deploy só com app

--- app.py
+++ app.py
@@ -2,20 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 from share import ShareToday
 
-fl = Flask(__name__)
-app = SocketIO(fl)
-server = app.server
+app = Flask(__name__)
+socketio = SocketIO(app)
 
 share_today = ShareToday(["Elielson", "Lamin", "Feitosa", "Geison", "Kelvin", "Tenis", "Iury"])
 
-@fl.route('/')
+@app.route('/')
 def home():
   worker = share_today.WhoShareToday()
   pending = share_today.PendingWorkers()
   return render_template("home.html", worker=worker, pending=pending)
 
-@app.on('next')
+@socketio.on('next')
 def next_request():
     worker=share_today.WhoShareToday(jump=True)
     data = {'worker': worker, 'pending': share_today.PendingWorkers()}
-    app.emit("nextWorker", data)
+    socketio.emit("nextWorker", data)
