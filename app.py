3846812f4c9062c94a4a2c9f6a877922430c119e
Elielson68 <elielsonbr.com@gmail.com>
2024-02-27 08:50:06 -0300

    feat: podendo pausar e salvar ordem
    Agora é possível pausar a vez para durar até outro dia, também agora a ordem de quem vai compartilhar é salva, então se o servidor cair e iniciar ele busca da ordem salva anteriormente.

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 from share import ShareToday
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-share_today = ShareToday(["Kelvin", "Tenis", "Jhone", "Iury", "Feitosa",
-                          "Campos", "Domingues", "Fagundes", "Elielson", "Lamin", "Geison", "Ruilan"])
+index = 0
+with open("share_data") as file:
+    index = int(file.read())
+    print("Index salvo: ", index)
 
+share_today = ShareToday(["Kelvin", "Tenis", "Jhone", "Iury", "Feitosa",
+                          "Campos", "Domingues", "Fagundes", "Elielson", "Lamin", "Geison", "Ruilan"],
+                         index)
 @app.route('/')
 def home():
-  worker = share_today.WhoShareToday()
-  pending = share_today.PendingWorkers()
-  return render_template("home.html", worker=worker, pending=pending)
+    worker = share_today.WhoShareToday()
+    pending = share_today.PendingWorkers()
+    print("EStá pausado? ", share_today.IsPaused())
+    return render_template("home.html", worker=worker, pending=pending, paused=share_today.IsPaused())
 
 
 @app.route('/reset')
 def reset():
-  share_today.Reset()
-  return "Index resetado!"
+    share_today.Reset()
+    return "Index resetado!"
 
 
 @socketio.on('next')
 def next_request():
-    worker=share_today.WhoShareToday(jump=True)
+    worker = share_today.WhoShareToday(jump=True)
     data = {'worker': worker, 'pending': share_today.PendingWorkers()}
     print("Recebeu a chamada de NEXT")
     socketio.emit("nextWorker", data)
+
+@socketio.on('pause')
+def pause_request():
+    print("Pediu para pausar")
+    share_today.RequestPause()
